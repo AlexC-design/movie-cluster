bae0775664f0264ffa2aecb1f30ea93056bb0e14
AlexC-Design <alex94.design@gmail.com>
2019-12-19 21:34:43 +0000

    testing cards animations

--- src/components/TestPage/TestComponent5.js
+++ src/components/TestPage/TestComponent5.js
@@ -11,8 +11,6 @@ class TestComponent5 extends Component {
   }
 
   componentDidMount = () => {
-    console.log(this.state.coords);
-
     const divs = [...document.querySelectorAll(".div")];
 
     divs.map(div => {
@@ -33,7 +31,9 @@ class TestComponent5 extends Component {
     console.log(this.state.coords);
   };
 
-  
+  dist = (x1, x2, y1, y2) => {
+    return Math.sqrt((x2 -= x1) * x2 + (y2 -= y1) * y2);
+  };
 
   render() {
     return (
@@ -44,9 +44,27 @@ class TestComponent5 extends Component {
             y1={this.state.coords.length ? this.state.coords[0].centerY : "100"}
             x2={this.state.coords.length ? this.state.coords[1].centerX : "100"}
             y2={this.state.coords.length ? this.state.coords[1].centerY : "100"}
+            style={{
+              strokeDasharray: this.state.coords.length
+                ? this.dist(
+                    this.state.coords[0].centerX,
+                    this.state.coords[1].centerX,
+                    this.state.coords[0].centerY,
+                    this.state.coords[1].centerY
+                  )
+                : 0,
+              strokeDashoffset: this.state.coords.length
+                ? this.dist(
+                    this.state.coords[0].centerX,
+                    this.state.coords[1].centerX,
+                    this.state.coords[0].centerY,
+                    this.state.coords[1].centerY
+                  )
+                : 0
+            }}
           />
         </svg>
-        <div className="div1 div"></div>
+        <div className="div1 div" style={{ top: "0vh", left: "60vw" }}></div>
         <div className="div2 div"></div>
       </>
     );
