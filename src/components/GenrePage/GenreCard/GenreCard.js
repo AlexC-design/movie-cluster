e0b06ce0a3d8fc2ee0150ba20759c19761a3a690
AlexC-Design <alex94.design@gmail.com>
2019-12-19 07:36:31 +0000

    minor changes to genre cards image sliders

--- src/components/GenrePage/GenreCard/GenreCard.js
+++ src/components/GenrePage/GenreCard/GenreCard.js
@@ -12,10 +12,9 @@ const GenreCard = ({ images, genre }) => {
 
   const [image1, setImage1] = useState(Math.floor(Math.random() * 20));
   const [image2, setImage2] = useState(Math.floor(Math.random() * 20));
-  const [position, setPosition] = useState([
-    "middle",
+  const [position, setPosition] = useState(
     indexToPosition[Math.floor(Math.random() * 4) + 1]
-  ]);
+  );
 
   useEffect(() => {
     var direction = Math.floor(Math.random() * 4) + 1;
@@ -23,7 +22,7 @@ const GenreCard = ({ images, genre }) => {
     var timeout = Math.floor(Math.random() * 5000) + 1000;
 
     setTimeout(() => {
-      setPosition([indexToPosition[0], indexToPosition[0]]);
+      setPosition(indexToPosition[0]);
     }, timeout);
 
     setTimeout(() => {
@@ -33,7 +32,7 @@ const GenreCard = ({ images, genre }) => {
     }, timeout * 1.5);
 
     setTimeout(() => {
-      setPosition([indexToPosition[0], indexToPosition[direction]]);
+      setPosition(indexToPosition[direction]);
     }, timeout * 2);
 
     setTimeout(() => {
@@ -46,13 +45,13 @@ const GenreCard = ({ images, genre }) => {
     <Link className="genre-card" to={`/genres/${genre.id}`}>
       <div className="images-container">
         <img
-          className={`poster ${position[0]}`}
+          className={"poster middle"}
           src={`http://image.tmdb.org/t/p/w342${
             images ? images[image1] : "UNDEF"
           }`}
         />
         <img
-          className={`poster ${position[1]}`}
+          className={`poster ${position}`}
           src={`http://image.tmdb.org/t/p/w342${
             images ? images[image2] : "UNDEF"
           }`}
