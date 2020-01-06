067d415a89958e550c201053387f35a90f3066c6
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-01-06 09:58:08 +0000

    Updates

---
+++ precache-manifest.b6eb5e184fbe6766436cba50497820e3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3fba90f6c2233a44ac9edad3734ace2",
+    "url": "/movie-cluster/index.html"
+  },
+  {
+    "revision": "6a7d6bc9bdb9d613c24a",
+    "url": "/movie-cluster/static/css/main.b0208cb0.chunk.css"
+  },
+  {
+    "revision": "778e7e822f3d5c2edaa8",
+    "url": "/movie-cluster/static/js/2.c5eea938.chunk.js"
+  },
+  {
+    "revision": "6a7d6bc9bdb9d613c24a",
+    "url": "/movie-cluster/static/js/main.d280ea0d.chunk.js"
+  },
+  {
+    "revision": "27b9f07a31c9661bfffb",
+    "url": "/movie-cluster/static/js/runtime-main.77ddd9a5.js"
+  },
+  {
+    "revision": "4f62ed17b82d99767955c02c64ef159e",
+    "url": "/movie-cluster/static/media/back-arrow-inverted-blue.4f62ed17.svg"
+  },
+  {
+    "revision": "66599917b692c3222a13f635216483ac",
+    "url": "/movie-cluster/static/media/back-arrow.66599917.svg"
+  },
+  {
+    "revision": "b083da19616d0ac021e1920790c45147",
+    "url": "/movie-cluster/static/media/down-arrow.b083da19.svg"
+  },
+  {
+    "revision": "1b308268c93b9f4074766c31405ff36d",
+    "url": "/movie-cluster/static/media/logo-circle.1b308268.svg"
+  },
+  {
+    "revision": "27d8c97777bcd048f9cfedcc4bbf8dc6",
+    "url": "/movie-cluster/static/media/logotype.27d8c977.svg"
+  },
+  {
+    "revision": "560f44fb393471f9af1b5ba6404fed1f",
+    "url": "/movie-cluster/static/media/search-logo.560f44fb.svg"
+  }
+]);
